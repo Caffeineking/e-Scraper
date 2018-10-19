6ea4744727e5638bb73093d659d1165128c78e0f
CaffeineKing <jacy_yahoo.com>
2018-10-18 21:51:18 -0700

    server8

--- public/app.js
+++ public/app.js
@@ -1,26 +1,22 @@
-
-
-
-  $(document).on("click", ".scrapeNow", function () {
+$(document).on("click", ".scrapeNow", function () {
     
-    console.log("you made it");
+  console.log("you made it");
 
+  $.ajax({
+    method: "GET",
+    url: "/scrape/"
+  }).then(function () {
     $.getJSON("/articles", function (data) {
-        console.log(data);
-      })
-      .then(function (data) {
-
-        for (var i = 0; i < data.length; i++) {
-          // Display the apropos information on the page
-          $("#articles").append("<div class='card'><p data-id='" +
-            data[i]._id + "'>" +
-            "<h5 class='card-header'>" + data[i].title +
-            "</br>" + "<a href='"+ data[i].link+"''class='btn btn-primary' target='_blank'>" + "Link" + "</a>" + "</p>");
-        }
-      })
-
-
+      console.log(data);
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#articles").append("<div class='card'><p data-id='" +
+          data[i]._id + "'>" +
+          "<h5 class='card-header'>" + data[i].title +
+          "</br>" + "<a href='"+ data[i].link+"''class='btn btn-primary' target='_blank'>" + "Link" + "</a>" + "</p>");
+      }
+    })
   })
-
-
-
+})
--- server.js
+++ server.js
@@ -45,41 +45,7 @@ mongoose.connect(MONGODB_URI);
 
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://esportsinsider.com/category/esports-titles/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-    
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("h3").each(function(i, element) { // I was not scraping right
-      // Save an empty result object
-      var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
-    });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
-  });
-});
 
 
 app.get("/scrape", function(req, res) {
