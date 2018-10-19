95bc1d248aed2ae89ffa120c877ebffc3e03b5a6
CaffeineKing <jacy_yahoo.com>
2018-10-18 19:22:07 -0700

    update mongoose db

---
+++ public/app.js
@@ -0,0 +1,26 @@
+
+
+
+  $(document).on("click", ".scrapeNow", function () {
+    
+    console.log("you made it");
+
+    $.getJSON("/articles", function (data) {
+        console.log(data);
+      })
+      .then(function (data) {
+
+        for (var i = 0; i < data.length; i++) {
+          // Display the apropos information on the page
+          $("#articles").append("<div class='card'><p data-id='" +
+            data[i]._id + "'>" +
+            "<h5 class='card-header'>" + data[i].title +
+            "</br>" + "<a href='"+ data[i].link+"''class='btn btn-primary' target='_blank'>" + "Link" + "</a>" + "</p>");
+        }
+      })
+
+
+  })
+
+
+
